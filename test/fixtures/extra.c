2fd8e01ff835327d7f3595076d2f9b0c1d304a41
Fedor Indutny <fedor@indutny.com>
2018-02-27 21:13:07 -0500

    lib: normalize for benchmarks

--- test/fixtures/extra.c
+++ test/fixtures/extra.c
@@ -1,6 +1,9 @@
 #include "fixture.h"
 
 int http_parser__on_url(llparse_state_t* s, const char* p, const char* endp) {
+  if (llparse__in_bench)
+    return 0;
+
   return llparse__print_span("url", p, endp);
 }
 
@@ -9,18 +12,24 @@ int http_parser__on_url(llparse_state_t* s, const char* p, const char* endp) {
 
 int http_parser__on_header_field(llparse_state_t* s, const char* p,
                                  const char* endp) {
+  if (llparse__in_bench)
+    return 0;
   return llparse__print_span("header_field", p, endp);
 }
 
 
 int http_parser__on_header_value(llparse_state_t* s, const char* p,
                                  const char* endp) {
+  if (llparse__in_bench)
+    return 0;
   return llparse__print_span("header_value", p, endp);
 }
 
 
 int http_parser__on_finished_header(llparse_state_t* s, const char* p,
                                     const char* endp) {
+  if (llparse__in_bench)
+    return 0;
   llparse__print(p, endp, "finished header");
   return 0;
 }
@@ -28,6 +37,8 @@ int http_parser__on_finished_header(llparse_state_t* s, const char* p,
 
 int http_parser__on_headers_complete(llparse_state_t* s, const char* p,
                                      const char* endp) {
+  if (llparse__in_bench)
+    return 0;
   llparse__print(p, endp, "headers complete");
   return 0;
 }
