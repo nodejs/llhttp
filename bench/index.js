54eaa6ec5fec2510a61cbb38ab02603783b08b20
Fedor Indutny <fedor@indutny.com>
2018-03-02 17:11:50 -0500

    lib: save progress

--- bench/index.js
+++ bench/index.js
@@ -3,38 +3,45 @@
 
 const spawnSync = require('child_process').spawnSync;
 
-console.log('url loose');
+const isURL = !process.argv[2] || process.argv[2] === 'url';
+const isHTTP = !process.argv[2] || process.argv[2] === 'http';
 
-spawnSync('./test/tmp/url-loose', [
-  'bench',
-  'http://example.com/path/to/file?query=value#fragment'
-], { stdio: 'inherit' });
+if (isURL) {
+  console.log('url loose');
 
-console.log('url strict');
+  spawnSync('./test/tmp/url-loose', [
+    'bench',
+    'http://example.com/path/to/file?query=value#fragment'
+  ], { stdio: 'inherit' });
 
-spawnSync('./test/tmp/url-strict', [
-  'bench',
-  'http://example.com/path/to/file?query=value#fragment'
-], { stdio: 'inherit' });
+  console.log('url strict');
 
-console.log('http loose');
+  spawnSync('./test/tmp/url-strict', [
+    'bench',
+    'http://example.com/path/to/file?query=value#fragment'
+  ], { stdio: 'inherit' });
+}
 
-spawnSync('./test/tmp/http-req-loose', [
-  'bench',
-  'GET /api/v1/path HTTP/1.1\r\n' +
-    'Header: Value\r\n' +
-    'Header1: Value1\r\n' +
-    'Header2: Value2\r\n' +
-    '\r\n'
-], { stdio: 'inherit' });
+if (isHTTP) {
+  console.log('http loose');
 
-console.log('http strict');
+  spawnSync('./test/tmp/http-req-loose', [
+    'bench',
+    'GET /api/v1/path HTTP/1.1\r\n' +
+      'Header: Value\r\n' +
+      'Header1: Value1\r\n' +
+      'Header2: Value2\r\n' +
+      '\r\n'
+  ], { stdio: 'inherit' });
 
-spawnSync('./test/tmp/http-req-strict', [
-  'bench',
-  'GET /api/v1/path HTTP/1.1\r\n' +
-    'Header: Value\r\n' +
-    'Header1: Value1\r\n' +
-    'Header2: Value2\r\n' +
-    '\r\n'
-], { stdio: 'inherit' });
+  console.log('http strict');
+
+  spawnSync('./test/tmp/http-req-strict', [
+    'bench',
+    'GET /api/v1/path HTTP/1.1\r\n' +
+      'Header: Value\r\n' +
+      'Header1: Value1\r\n' +
+      'Header2: Value2\r\n' +
+      '\r\n'
+  ], { stdio: 'inherit' });
+}
