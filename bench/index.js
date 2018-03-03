e11142daa0bc7747700c7795fd5e326fa8b27053
Fedor Indutny <fedor@indutny.com>
2018-03-02 19:50:24 -0500

    bench: similar input to http-parser

--- bench/index.js
+++ bench/index.js
@@ -6,6 +6,22 @@ const spawnSync = require('child_process').spawnSync;
 const isURL = !process.argv[2] || process.argv[2] === 'url';
 const isHTTP = !process.argv[2] || process.argv[2] === 'http';
 
+const REQUEST =
+  'POST /joyent/http-parser HTTP/1.1\r\n' +
+  'Host: github.com\r\n' +
+  'DNT: 1\r\n' +
+  'Accept-Encoding: gzip, deflate, sdch\r\n' +
+  'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4\r\n' +
+  'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' +
+      'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+      'Chrome/39.0.2171.65 Safari/537.36\r\n' +
+  'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,' +
+      'image/webp,*/*;q=0.8\r\n' +
+  'Referer: https://github.com/joyent/http-parser\r\n' +
+  'Connection: keep-alive\r\n' +
+  'Transfer-Encoding: chunked\r\n' +
+  'Cache-Control: max-age=0\r\n\r\n';
+
 if (isURL) {
   console.log('url loose');
 
@@ -27,21 +43,13 @@ if (isHTTP) {
 
   spawnSync('./test/tmp/http-req-loose', [
     'bench',
-    'GET /api/v1/path HTTP/1.1\r\n' +
-      'Header: Value\r\n' +
-      'Header1: Value1\r\n' +
-      'Header2: Value2\r\n' +
-      '\r\n'
+    REQUEST
   ], { stdio: 'inherit' });
 
   console.log('http strict');
 
   spawnSync('./test/tmp/http-req-strict', [
     'bench',
-    'GET /api/v1/path HTTP/1.1\r\n' +
-      'Header: Value\r\n' +
-      'Header1: Value1\r\n' +
-      'Header2: Value2\r\n' +
-      '\r\n'
+    REQUEST
   ], { stdio: 'inherit' });
 }
