5b0d0301c899ad71d23f990a7698be4550bcb460
Robert Nagy <ronagy@icloud.com>
2021-06-08 09:53:25 +0200

    pass more info in wasm_on_header_complete
    Avoids the need for additional calls into wasm.

--- src/native/api.c
+++ src/native/api.c
@@ -50,13 +50,18 @@ extern int wasm_on_headers_complete(llhttp_t * p);
 extern int wasm_on_body(llhttp_t* p, const char* at, size_t length);
 extern int wasm_on_message_complete(llhttp_t * p);
 
+static int wasm_on_headers_complete_wrap(llhttp_t* p) {
+  return wasm_on_headers_complete(p, p->status_code, p->upgrade,
+                                  llhttp_should_keep_alive(p));
+}
+
 const llhttp_settings_t wasm_settings = {
   wasm_on_message_begin,
   wasm_on_url,
   wasm_on_status,
   wasm_on_header_field,
   wasm_on_header_value,
-  wasm_on_headers_complete,
+  wasm_on_headers_complete_wrap,
   wasm_on_body,
   wasm_on_message_complete,
   NULL,
