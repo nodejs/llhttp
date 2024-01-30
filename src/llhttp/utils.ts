30055f41c7925c557d26d84903c3135ef1e9a239
uzlopak <aras.abbasi@googlemail.com>
2024-01-30 09:21:22 +0100

    fix linting

--- src/llhttp/utils.ts
+++ src/llhttp/utils.ts
@@ -1,12 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyObject = Record<string, any>;
+
 type NumericKeys<T> = {
   [K in keyof T]: T[K] extends number ? K : never
 }[keyof T];
 
-export type IEnumMap<T = Record<string, any>> = {
+export type IEnumMap<T = AnyObject> = {
   [K in NumericKeys<T>]: T[K]
 };
 
-export function enumToMap<T extends Record<string, any>>(
+export function enumToMap<T extends AnyObject>(
   obj: T,
   filter?: ReadonlyArray<number>,
   exceptions?: ReadonlyArray<number>,
