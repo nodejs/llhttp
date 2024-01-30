a3186f71d06b5b57a3742f1473dc63cd232a8482
Paolo Insogna <paolo@cowtech.it>
2024-01-30 11:22:54 +0100

    feat: Get rid of enums. (#287)
    * feat: Get rid of enums.
    
    * fix: Linted code.
    
    * chore: Renamed type.

--- src/llhttp/utils.ts
+++ src/llhttp/utils.ts
@@ -1,34 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AnyObject = Record<string, any>;
+import { IntDict } from './constants';
 
-type NumericKeys<T> = {
-  [K in keyof T]: T[K] extends number ? K : never
-}[keyof T];
+export function enumToMap(
+  obj: IntDict,
+  filter: ReadonlyArray<number> = [],
+  exceptions: ReadonlyArray<number> = [],
+): IntDict {
+  const emptyFilter = (filter?.length ?? 0) === 0;
+  const emptyExceptions = (exceptions?.length ?? 0) === 0;
 
-export type IEnumMap<T = AnyObject> = {
-  [K in NumericKeys<T>]: T[K]
-};
-
-export function enumToMap<T extends AnyObject>(
-  obj: T,
-  filter?: ReadonlyArray<number>,
-  exceptions?: ReadonlyArray<number>,
-): IEnumMap<T> {
-  const res: Record<string, number> = {};
-
-  for (const key of Object.keys(obj)) {
-    const value = obj[key];
-    if (typeof value !== 'number') {
-      continue;
-    }
-    if (filter && !filter.includes(value)) {
-      continue;
-    }
-    if (exceptions && exceptions.includes(value)) {
-      continue;
-    }
-    res[key] = value;
-  }
-
-  return res as IEnumMap<T>;
+  return Object.fromEntries(Object.entries(obj).filter(([ , value ]) => {
+    return (
+      typeof value === 'number' &&
+      (emptyFilter || filter.includes(value)) &&
+      (emptyExceptions || !exceptions.includes(value))
+    );
+  }));
 }
