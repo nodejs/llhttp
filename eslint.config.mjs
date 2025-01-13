9e7ecac89e0112cd6c3fc81de695888de58116f7
Paolo Insogna <paolo@cowtech.it>
2025-01-13 09:45:56 +0100

    chore: Updated toolchain.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,66 +1,47 @@
-import stylisticJs from '@stylistic/eslint-plugin-js'
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import globals from 'globals';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
-  { ignores: ["build", "lib", "examples", "bench"] },
+  { 
+    ignores: ["build", "lib", "examples", "bench", "eslint.config.mjs"] 
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  ...tseslint.configs.stylistic,
   {
-    "languageOptions": {
-      "parser": tseslint.parser,
-      "parserOptions": {
-        "lib": ["es2023"],
-        "module": "commonjs",
-        "moduleResolution": "node",
-        "target": "es2022",
-    
-        "strict": true,
-        "esModuleInterop": true,
-        "skipLibCheck": true,
-        "include": [
-          "bin/**/*.ts",
-          "src/**/*.ts",
-          "test/**/*.ts"
-        ],
-        "outDir": "./lib",
-        "declaration": true,
-        "pretty": true,
-        "sourceMap": true
-      },
-      "globals": {
-        ...globals.commonjs,
-        ...globals.node,
-        ...globals.es6
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true
       },
     },
   },
   {
     plugins: {
-      '@stylistic/js': stylisticJs
+      "@stylistic": stylistic,
     },
-    files: [
-      "bin/**/*.ts",
-      'bench/**/*.ts',
-      'src/**/*.ts',
-      'test/**/*.ts',
-    ],
+    files: ["bin/**/*.ts", "bench/**/*.ts", "src/**/*.ts", "test/**/*.ts"],
     rules: {
-    '@stylistic/js/max-len': [ 2, {
-      'code': 120,
-      'ignoreComments': true
-    } ],
-      "@stylistic/js/array-bracket-spacing": ["error", "always"],
-      "@stylistic/js/operator-linebreak": ["error", "after"],
-      "@stylistic/js/linebreak-style": ["error", "unix"],
-      "@stylistic/js/brace-style": ["error", "1tbs", { "allowSingleLine": true }],
-      '@stylistic/js/indent': ["error", 2, {
-        "SwitchCase": 1,
-        "FunctionDeclaration":  { "parameters": "first" },
-        "FunctionExpression": { "parameters": "first" }
-      }],
-    }
+      "@stylistic/max-len": [
+        2,
+        {
+          code: 120,
+          ignoreComments: true,
+        },
+      ],
+      "@stylistic/array-bracket-spacing": ["error", "always"],
+      "@stylistic/operator-linebreak": ["error", "after"],
+      "@stylistic/linebreak-style": ["error", "unix"],
+      "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+      "@stylistic/indent": [
+        "error",
+        2,
+        {
+          SwitchCase: 1,
+          FunctionDeclaration: { parameters: "first" },
+          FunctionExpression: { parameters: "first" },
+        },
+      ],
+    },
   }
 );
