85e2c559c3132df8d330910ce6f35f2abfabbbf5
Paolo Insogna <paolo@cowtech.it>
2025-01-13 10:22:03 +0100

    fixup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["lib"],
+    ignores: ["build", "lib"],
   },
   {
     files: [
